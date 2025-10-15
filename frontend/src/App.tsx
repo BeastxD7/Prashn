bb124dd6f731ba57a7f00c1ed4fafd5ddb18ca40
Shashank Devadiga <beast.steam002@gmail.com>
2025-10-15 16:06:37 +0530

    Navigation completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { ThemeProvider } from "@/components/theme-provider"
-import { ModeToggle } from "@/components/mode-toggle"
+import { NavbarDemo } from "./components/navigation/Navbar"
 function App() {
 
 
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <ModeToggle />
+        <NavbarDemo/>
       </ThemeProvider>
     </>
   )
---
+++ frontend/src/constants/Navbar.ts
@@ -0,0 +1,15 @@
+
+export const navItems = [
+    {
+        name: "Features",
+        link: "#features",
+    },
+    {
+        name: "Pricing",
+        link: "#pricing",
+    },
+    {
+        name: "Contact",
+        link: "#contact",
+    },
+];
